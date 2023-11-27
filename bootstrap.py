a3ce045400280459c4c22a5116aec8a696731ca6
DavidYordan <xinburuquan@gmail.com>
2023-11-27 08:13:06 +0800

    update

--- bootstrap.py
+++ bootstrap.py
@@ -1,9 +1,11 @@
+import ctypes
 import os
 import shutil
 import subprocess
 import sys
 import tempfile
 import tkinter as tk
+from ctypes import wintypes
 from tkinter import filedialog
 
 def create_directory(path):
@@ -12,18 +14,18 @@ def create_directory(path):
         os.makedirs(final_path)
     return final_path
 
-def create_shortcut_with_vbscript(target, shortcut_name):
-    desktop_path = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
+def create_shortcut_with_vbscript(target, shortcut_name, shortcut_dir):
     vbscript = f"""
     Set oWS = WScript.CreateObject("WScript.Shell")
-    sLinkFile = "{desktop_path}\\{shortcut_name}.lnk"
+    sLinkFile = "{shortcut_dir}\\{shortcut_name}.lnk"
     Set oLink = oWS.CreateShortcut(sLinkFile)
     oLink.TargetPath = "{target}"
     oLink.Save
     """
-    with tempfile.NamedTemporaryFile('w', delete=False, suffix='.vbs') as vbs:
+    vbs_path = tempfile.mktemp(suffix='.vbs')
+    with open(vbs_path, 'w') as vbs:
         vbs.write(vbscript)
-        vbs_path = vbs.name
+
     subprocess.run(["cscript", vbs_path], shell=True)
     os.remove(vbs_path)
 
@@ -48,6 +50,14 @@ def extract_resources(source_dir, target_dir):
         else:
             shutil.copytree(full_source_path, full_target_path)
 
+def get_desktop_path():
+    CSIDL_DESKTOP = 0
+    SHGFP_TYPE_CURRENT = 0
+    MAX_PATH = 260
+    path_buf = ctypes.create_unicode_buffer(MAX_PATH)
+    ctypes.windll.shell32.SHGetFolderPathW(None, CSIDL_DESKTOP, None, SHGFP_TYPE_CURRENT, path_buf)
+    return path_buf.value
+
 def main():
     chosen_dir = choose_directory()
     if not chosen_dir:
@@ -66,7 +76,8 @@ def main():
     extract_resources(base_path, working_dir)
 
     exe_path = os.path.join(working_dir, 'V2XrayMultiMapper.exe')
-    create_shortcut_with_vbscript(exe_path, 'V2XrayMultiMapper')
+    
+    create_shortcut_with_vbscript(exe_path, 'V2XrayMultiMapper', get_desktop_path())
 
     subprocess.Popen([exe_path])
 
--- main.py
+++ main.py
@@ -25,8 +25,12 @@ from routing_tab import RoutingTab
 class V2XrayMultiMapper(QMainWindow):
     def __init__(self):
         super().__init__()
+        
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        os.chdir(current_dir)
+
         self.setWindowTitle("V2XrayMultiMapper")
-        self.setWindowIcon(QIcon('img/logo.ico'))
+        self.setWindowIcon(QIcon('img/V2XrayMultiMapper.ico'))
         self.resize(1040, 620)
         self.create_menu()
         self.create_main_panel()
